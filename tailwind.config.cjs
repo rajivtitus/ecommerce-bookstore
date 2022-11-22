d210b98e654550d50d675f13aa0415b1c40a7c4c
Rajiv Titus <75184965+rajivtitus@users.noreply.github.com>
2022-11-21 19:09:57 -0800

    setup primary fonts and colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
 	content: ["./src/**/*.{js,jsx,ts,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				lora: " 'Lora', serif ",
+				sans: ["'Open Sans'", ...defaultTheme.fontFamily.sans],
+			},
+			colors: {
+				primary: "#043E85",
+				secondary: "#DDB57C",
+			},
+		},
 	},
 	plugins: [],
 };
