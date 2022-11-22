7f61645c5185ec7778305d63535bed676c037dfe
Rajiv Titus <75184965+rajivtitus@users.noreply.github.com>
2022-11-21 16:10:06 -0800

    setup basic redux store

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import "./App.css";
+import { useAppSelector, useAppDispatch } from "./app/hooks";
+import { increment } from "./features/counter/counterSlice";
 
 function App() {
+	const count = useAppSelector((state) => state.counter.value);
+	const dispatch = useAppDispatch();
+
+	const handleCount = () => {
+		dispatch(increment());
+	};
+
 	return (
 		<div className="App">
 			<p>Bookworm The E-commerce Bookstore</p>
+			<button className="bg-blue-500 py-2 px-4 rounded" onClick={handleCount}>
+				Count is {count}
+			</button>
 		</div>
 	);
 }
