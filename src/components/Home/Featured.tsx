4d28a52f81d8ff2c42820b6cebb9488eac03812c
Rajiv Titus <75184965+rajivtitus@users.noreply.github.com>
2022-11-23 11:26:25 -0800

    helper to get month name

--- src/components/Home/Featured.tsx
+++ src/components/Home/Featured.tsx
@@ -1,9 +1,15 @@
 import { ReactElement } from "react";
 
+import { getMonthName } from "../../utils/helpers";
+
 const Featured = (): ReactElement => {
+	const currMonth = getMonthName();
+
 	return (
 		<div className="min-h-screen py-4">
-			<h2 className="py-2 mb-8 font-lora text-3xl">Featured</h2>
+			<h2 className="py-2 mb-8 font-lora text-3xl">
+				{currMonth}'s Featured Author
+			</h2>
 		</div>
 	);
 };
--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,8 +1,28 @@
 import { CartItem } from "../utils/types";
 
+const MONTHS = [
+	"January",
+	"February",
+	"March",
+	"April",
+	"May",
+	"June",
+	"July",
+	"August",
+	"September",
+	"October",
+	"November",
+	"December",
+];
+
 export const calcCartTotal = (items: CartItem[]): number => {
 	return items.reduce((sum, cur: CartItem) => {
 		const { price, quantity } = cur;
 		return (sum += price * quantity);
 	}, 0);
+};
+
+export const getMonthName = (): string => {
+	const currMonth = new Date().getMonth();
+	return MONTHS[currMonth];
 };
