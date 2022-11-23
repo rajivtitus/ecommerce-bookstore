db7b25052a0a2065b43208f78b8f7b1487d5549e
Rajiv Titus <75184965+rajivtitus@users.noreply.github.com>
2022-11-23 11:57:32 -0800

    added alt text to pages

--- src/components/Home/Featured.tsx
+++ src/components/Home/Featured.tsx
@@ -3,6 +3,7 @@ import { ReactElement } from "react";
 import { getMonthName } from "../../utils/helpers";
 
 const Featured = (): ReactElement => {
+	const author = null;
 	const currMonth = getMonthName();
 
 	return (
@@ -10,6 +11,17 @@ const Featured = (): ReactElement => {
 			<h2 className="py-2 mb-8 font-lora text-3xl">
 				{currMonth}'s Featured Author
 			</h2>
+			{author ? (
+				<div>
+					<p>Author exists</p>
+				</div>
+			) : (
+				<div className="py-16 text-center">
+					<h4 className="mb-8 text-2xl ">
+						No authors found, please try again later.
+					</h4>
+				</div>
+			)}
 		</div>
 	);
 };
--- src/routes/Products.tsx
+++ src/routes/Products.tsx
@@ -10,11 +10,19 @@ const Products = (): ReactElement => {
 	return (
 		<Layout>
 			<h2 className="py-2 mb-8 text-center font-lora text-3xl">Products</h2>
-			<div className="grid grid-cols-2 content-center xl:grid-cols-3 gap-16 px-6 pb-12">
-				{books.map((book) => (
-					<ProductCard book={book} key={book.id} />
-				))}
-			</div>
+			{books.length ? (
+				<div className="grid grid-cols-2 content-center xl:grid-cols-3 gap-16 px-6 pb-12">
+					{books.map((book) => (
+						<ProductCard book={book} key={book.id} />
+					))}
+				</div>
+			) : (
+				<div className="py-16 text-center">
+					<h4 className="mb-8 text-2xl ">
+						No products found, please try again later.
+					</h4>
+				</div>
+			)}
 		</Layout>
 	);
 };
