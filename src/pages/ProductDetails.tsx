9c61fa6a850b58656c7f4a1971032bbf31fc0370
Rajiv Titus <75184965+rajivtitus@users.noreply.github.com>
2022-11-24 07:20:55 -0800

    added alt text for product details page

--- src/pages/ProductDetails.tsx
+++ src/pages/ProductDetails.tsx
@@ -5,21 +5,34 @@ import Layout from "../components/Layout";
 import ProductInfo from "../components/ProductDetails/ProductInfo";
 import { useAppSelector } from "../app/hooks";
 import LoadingSpinner from "../components/LoadingSpinner";
+import ErrorPage from "./ErrorPage";
 
 type ProductParams = {
-  id?: string;
+	id?: string;
 };
 
 const ProductDetails = (): ReactElement => {
-  const { id } = useParams<ProductParams>();
-  const { books, isLoading } = useAppSelector((state) => state.products);
-  const book = books.filter((book) => book.id === Number(id))[0];
+	const { id } = useParams<ProductParams>();
+	const { books, isLoading } = useAppSelector((state) => state.products);
+	const book = books.filter((book) => book.id === Number(id))[0];
 
-  return (
-    <Layout>
-      {!isLoading ? book && <ProductInfo book={book} /> : <LoadingSpinner />}
-    </Layout>
-  );
+	return (
+		<Layout>
+			{!isLoading ? (
+				book ? (
+					<ProductInfo book={book} />
+				) : (
+					<div className="py-16 text-center">
+						<h4 className="mb-8 text-2xl ">
+							No product found, please try again later.
+						</h4>
+					</div>
+				)
+			) : (
+				<LoadingSpinner />
+			)}
+		</Layout>
+	);
 };
 
 export default ProductDetails;
