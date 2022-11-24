8f01ec1331f7aa34917a85762318489bec8ab03d
Rajiv Titus <75184965+rajivtitus@users.noreply.github.com>
2022-11-24 00:26:12 -0800

    created loading spinner

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,18 +5,28 @@ import Featured from "../components/Home/Featured";
 import Landing from "../components/Home/Landing";
 
 import logo from "../assets/logo.png";
+import { useAppSelector } from "../app/hooks";
+import LoadingSpinner from "../components/LoadingSpinner";
 
 const Home = (): ReactElement => {
-  return (
-    <Layout>
-      <header className="flex justify-center items-center gap-4 mb-6">
-        <img src={logo} alt="logo" />
-        <h1 className="py-2 text-center font-lora text-3xl">Bookworm</h1>
-      </header>
-      <Landing />
-      <Featured />
-    </Layout>
-  );
+	const isLoading = useAppSelector((state) => state.products.isLoading);
+
+	return (
+		<Layout>
+			{!isLoading ? (
+				<>
+					<header className="flex justify-center items-center gap-4 mb-6">
+						<img src={logo} alt="logo" />
+						<h1 className="py-2 text-center font-lora text-3xl">Bookworm</h1>
+					</header>
+					<Landing />
+					<Featured />
+				</>
+			) : (
+				<LoadingSpinner />
+			)}
+		</Layout>
+	);
 };
 
 export default Home;
--- src/pages/ProductDetails.tsx
+++ src/pages/ProductDetails.tsx
@@ -4,18 +4,22 @@ import { useParams } from "react-router-dom";
 import Layout from "../components/Layout";
 import ProductInfo from "../components/ProductDetails/ProductInfo";
 import { useAppSelector } from "../app/hooks";
+import LoadingSpinner from "../components/LoadingSpinner";
 
 type ProductParams = {
-  id?: string;
+	id?: string;
 };
 
 const ProductDetails = (): ReactElement => {
-  const { id } = useParams<ProductParams>();
-  const book = useAppSelector(
-    (state) => state.products.books.filter((book) => book.id === Number(id))[0]
-  );
+	const { id } = useParams<ProductParams>();
+	const { books, isLoading } = useAppSelector((state) => state.products);
+	const book = books.filter((book) => book.id === Number(id))[0];
 
-  return <Layout>{book && <ProductInfo book={book} />}</Layout>;
+	return (
+		<Layout>
+			{!isLoading ? book && <ProductInfo book={book} /> : <LoadingSpinner />}
+		</Layout>
+	);
 };
 
 export default ProductDetails;
