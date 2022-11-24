411a52388a76bc0f3974a1ee1e40fce62bf693be
Rajiv Titus <75184965+rajivtitus@users.noreply.github.com>
2022-11-23 19:41:37 -0800

    added classnames pkg

--- src/components/Navbar/NavItem.tsx
+++ src/components/Navbar/NavItem.tsx
@@ -1,35 +1,34 @@
 import { ReactElement } from "react";
 import { Link } from "react-router-dom";
+import classNames from "classnames";
 
 import { NavLink, Paths } from "../../utils/types";
 
 interface Props {
-  link: NavLink;
-  path: string;
-  cartCount: number;
+	link: NavLink;
+	path: string;
+	cartCount: number;
 }
 
 const badgeStyles =
-  "inline-flex absolute -top-2 -right-2 justify-center items-center w-4 h-4 text-xs font-bold text-white bg-secondary rounded-full";
+	"inline-flex absolute -top-2 -right-2 justify-center items-center w-4 h-4 text-xs font-bold text-white bg-secondary rounded-full";
+
+const linkStyles = "bg-primary absolute h-[2px] w-full bottom-0 left-0";
 
 const NavItem = ({ link, path, cartCount }: Props): ReactElement => {
-  const isCart = link.path === Paths.Cart;
+	const isCart = link.path === Paths.Cart;
 
-  return (
-    <li className="px-2 pb-1 mr-12 text-lg font-lora relative cursor-pointer">
-      {isCart && cartCount ? (
-        <span className={isCart ? badgeStyles : ""}>{cartCount}</span>
-      ) : null}
-      <Link to={link.path}>{link.name}</Link>
-      <span
-        className={
-          path === link.path
-            ? `bg-primary absolute h-[2px] w-full bottom-0 left-0`
-            : ""
-        }
-      />
-    </li>
-  );
+	return (
+		<li className="px-2 pb-1 mr-12 text-lg font-lora relative cursor-pointer">
+			{isCart && cartCount ? (
+				<span className={classNames({ [badgeStyles]: isCart })}>
+					{cartCount}
+				</span>
+			) : null}
+			<Link to={link.path}>{link.name}</Link>
+			<span className={classNames({ [linkStyles]: link.path === path })} />
+		</li>
+	);
 };
 
 export default NavItem;
